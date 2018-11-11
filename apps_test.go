3882e349a7de2534f9690de9bb97a66fa919d087
hussam <hussam@jafar.io>
2018-11-10 20:59:22 -0500

    Setup basic testing

---
+++ apps_test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"github.com/dustinkirkland/golang-petname"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestApp_Create(t *testing.T) {
+	name := "test-" + petname.Name()
+	tests := []struct {
+		name         string
+		ShouldCreate bool
+	}{
+		{name, true},
+		{name, false},
+	}
+	for _, test := range tests {
+		err := App{test.name}.Create()
+		if test.ShouldCreate == true {
+			assert.Nil(t, err, "Namespace should be created")
+		} else {
+			assert.NotNil(t, err, "Namespace shouldn't be created")
+		}
+	}
+}
